2ec168fdf8020b68ea0acae01e3ef2e429a6b46c
pymaster9 <adi31tya@icloud.com>
2024-07-24 14:36:35 -0400

    Learning how to delete files from inside zipfile

--- ImageDataset.py
+++ ImageDataset.py
@@ -13,30 +13,31 @@ from PIL import Image
 import pickle
 
 # Import zip file
-def import_images(path):
-   realimgs = []
-   fakeimgs = []
-   with zipfile.ZipFile(path, 'r') as zip:
-       for file in zip.namelist():
-           if "real" in file.lower():
-                try:
-                   image_file = zip.open(file)
-                   image_data = image_file.read()
-                   image = Image.open(BytesIO(image_data))
-                   image = image.crop((0,0,image.size[0]-98, image.size[1]-20)).resize((500,500))
-                   realimgs.append(image)
-                except:
-                    pass
-           if "fake" in file.lower():
-               try:
-                   image_file = zip.open(file)
-                   image_data = image_file.read()
-                   image = Image.open(BytesIO(image_data))
-                   image = image.crop((0,0,image.size[0]-98, image.size[1]-20)).resize((500,500))
-                   fakeimgs.append(image)
-               except:
-                   pass
-   return (realimgs, fakeimgs)
+def import_images(paths):
+    realimgs = []
+    fakeimgs = []
+    for path in paths:
+        with zipfile.ZipFile(path, 'r') as zip:
+            for file in zip.namelist():
+                if "real" in file.lower():
+                        try:
+                            image_file = zip.open(file)
+                            image_data = image_file.read()
+                            image = Image.open(BytesIO(image_data))
+                            image = image.crop((0,0,image.size[0]-98, image.size[1]-20)).resize((500,500))
+                            realimgs.append(image)
+                        except:
+                            pass
+                if "fake" in file.lower():
+                    try:
+                        image_file = zip.open(file)
+                        image_data = image_file.read()
+                        image = Image.open(BytesIO(image_data))
+                        image = image.crop((0,0,image.size[0]-98, image.size[1]-20)).resize((500,500))
+                        fakeimgs.append(image)
+                    except:
+                        pass
+    return (realimgs, fakeimgs)
 
 # Define the dataset class
 class ImageDataset(Dataset):
@@ -64,8 +65,10 @@ class ImageDataset(Dataset):
 if __name__ == "__main__":
     #Run ONLY once!
 
-    realimgs, fakeimgs = import_images("/Users/aditya/Desktop/Training Dataset.zip")
+    realimgs, fakeimgs = import_images(["TrainingDataset1.zip", "TrainingDataset1.zip", "TrainingDataset3.zip"])
     with open("realimgs", "wb") as txt: 
         pickle.dump(realimgs, txt)
     with open("fakeimgs", "wb") as txt: 
         pickle.dump(fakeimgs, txt)
+    print(os.path.getsize("realimgs"))
+    print(os.path.getsize("fakeimgs"))
